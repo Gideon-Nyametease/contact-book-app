36dea4bf01b5731087e895216a284e303e68529c
Gideon Nyametease <gknyametease@gmail.com>
2020-09-04 12:32:14 +0000

    9th attempt

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Button } from 'antd';
+
+function App() {
+  return (
+   <Button type="primary">Add</Button>
+  );
+}
+
+export default App;
